58c7c0674875886d2709e3b5278dc1c7866e072d
UmaShiny <221205015@ccmailg.meijo-u.ac.jp>
2025-10-15 22:56:54 +0900

    script createrの統合

--- scripts/get_from_nfq/script_creater.py
+++ scripts/get_from_nfq/script_creater.py
@@ -1,33 +1,32 @@
 import re
 import pandas as pd
 
-path_to_src_nfq = ".\\src_nfq\\AutoRun_exapmle_copy.nfq" # このnfqファイルを参照して，必要な修正を実施後，dst_nfqに出力する
+path_to_src_nfq = "data/nfq/nfq_src/AutoRun_sample.nfq" # このnfqファイルを参照して，必要な修正を実施後，dst_nfqに出力する
 # path_to_dst_nfq = ".\\dst_nfq\\AutoRun_Allitem.nfq" # src_nfqを修正した内容を蓄積して，最終的に全社分のデータを取得できる自動実行スクリプトを作成する
-path_to_datasrc_csv = ".\\csv\\Ticker_JpName_EnName.csv" # データを取得したい企業を一意に特定するための基本データが入ってるcsvファイル
+path_to_datasrc_csv = "data/tickers/result/Tickers_with_Nikkei_copy.csv" # データを取得したい企業を一意に特定するための基本データが入ってるcsvファイル
 
 src_csv_encoding = "shift_jis"
 src_nfq_encoding = "utf-8"
 
-dst_TickerHead_Memory:int = 10
+dst_TickerHead_Memory:int = 1
 
 datasrc_csv = pd.read_csv(path_to_datasrc_csv, encoding=src_csv_encoding)
-dst_nfq_tickerList:list[str] = datasrc_csv["TICKER"].tolist()
-dst_nfq_JpNameList:list[str] = datasrc_csv["CORPORATE_CCANAME"].tolist()
-dst_nfq_EnNameList:list[str] = datasrc_csv["CORPORATE_ENAME1"].tolist()
-zipped_dst_nfq_Ticker_JpName_EnName_List = list(zip(dst_nfq_tickerList, dst_nfq_JpNameList, dst_nfq_EnNameList))
+dst_nfq_nikkeiList:list[str] = datasrc_csv["nikkei"].tolist()
+dst_nfq_tickerList:list[str] = datasrc_csv["ticker"].tolist()
+dst_nfq_JpNameList:list[str] = datasrc_csv["ja"].tolist()
+dst_nfq_EnNameList:list[str] = datasrc_csv["eng"].tolist()
+zipped_dst_nfq_Ticker_JpName_EnName_List = list(zip(dst_nfq_nikkeiList, dst_nfq_tickerList, dst_nfq_JpNameList, dst_nfq_EnNameList))
 
 # src_nfqの置き換え対象を変数として定義する
-src_nfq_Ticker:str = "src_nfq_Ticker"
-src_nfq_JpName:str = "src_nfq_JpName"
-src_nfq_EnName:str = "src_nfq_EnName"
+src_nfq_Nikkei:str = "Ticker"
+src_nfq_JpName:str = "JpName"
+src_nfq_EnName:str = "EnName"
 
 SRC_PATTERN_TUPLE_INEDX, DST_REPLACE_TUPLE_INEDX = 0, 1
-for dst_nfq_Ticker, dst_nfq_JpName, dst_nfq_EnName in zipped_dst_nfq_Ticker_JpName_EnName_List:
-    if not dst_nfq_Ticker[1].isdigit(): continue
-    dst_TickerHead = int(dst_nfq_Ticker[1:3])
-    if dst_TickerHead > dst_TickerHead_Memory: dst_TickerHead_Memory = dst_TickerHead
-    path_to_dst_nfq = f".\\dst_nfq\\AutoRun_Allitem_{dst_TickerHead_Memory}_test.nfq"
-    src_to_dst_replace_tuple:tuple[tuple[str,str], ...] = ((src_nfq_Ticker, dst_nfq_Ticker), # modify_order = 0
+for dst_nfq_nikkei, dst_nfq_ticker, dst_nfq_JpName, dst_nfq_EnName in zipped_dst_nfq_Ticker_JpName_EnName_List:
+    path_to_dst_nfq = f"data/nfq/nfq_dst/AutoRun_Allitem_{dst_nfq_ticker}.nfq"
+    print("\r", f"Now creating {path_to_dst_nfq} ...", end="")
+    src_to_dst_replace_tuple:tuple[tuple[str,str], ...] = ((src_nfq_Nikkei, dst_nfq_nikkei), # modify_order = 0
                                                            (src_nfq_JpName, dst_nfq_JpName), # modify_order = 1
                                                            (src_nfq_EnName, dst_nfq_EnName)) # modify_order = 2
     with open(path_to_dst_nfq, "ab") as dst_nfq_fp:
@@ -35,7 +34,9 @@ for dst_nfq_Ticker, dst_nfq_JpName, dst_nfq_EnName in zipped_dst_nfq_Ticker_JpNa
             src_nfq_all_content = src_nfq_fp.read()
             dst_nfq_all_content = src_nfq_all_content
             for modify_order in range(len(src_to_dst_replace_tuple)):
-                dst_nfq_all_content = dst_nfq_all_content.replace(src_to_dst_replace_tuple[modify_order][SRC_PATTERN_TUPLE_INEDX].encode(src_nfq_encoding),
-                                                                  src_to_dst_replace_tuple[modify_order][DST_REPLACE_TUPLE_INEDX].encode(src_csv_encoding),)
+                dst_nfq_all_content = dst_nfq_all_content.replace(
+                    src_to_dst_replace_tuple[modify_order][SRC_PATTERN_TUPLE_INEDX].encode(src_nfq_encoding),
+                    src_to_dst_replace_tuple[modify_order][DST_REPLACE_TUPLE_INEDX].encode(src_csv_encoding),
+                )
             dst_nfq_fp.write(dst_nfq_all_content)
-print("The AutoRun script has been generated")
+print("\nThe AutoRun script has been generated")
