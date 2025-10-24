8cc6668cf4d1eafd39765650b37e1fcc17f2e806
UmaShiny <221205015@ccmailg.meijo-u.ac.jp>
2025-10-24 14:23:16 +0900

    chnage config to get fix-xlsx about stc-vol

--- scripts/nfq2xlsx/Config.py
+++ scripts/nfq2xlsx/Config.py
@@ -6,13 +6,13 @@ class Path:
     TICKER_SYMBOL_CSV_PATH = "scripts/nfq2xlsx/TickerOnly.csv"
 
     SRC_DIRPATH_DICT = {
-        nfq.repair_nfqxlsx.__name__: "data/nfq/raw_nfqxlsx",  # must be remain!!
-        nfq.xlsx2csv.__name__: "data/nfq/concat_nfqxlsx",
+        nfq.repair_nfqxlsx.__name__: "data/nfq/raw_nfqxlsx/raw_stcvol_nfqxlsx",  # must be remain!!
+        nfq.xlsx2csv.__name__: "data/nfq/concat_nfqxlsx/concat_stcvol_nfqxlsx",
     }
 
     DST_DIRPATH_DICT = {
-        nfq.repair_nfqxlsx.__name__: "data/nfq/fix_nfqxlsx",
-        nfq.xlsx2csv.__name__: "data/nfq/nfqcsv",
+        nfq.repair_nfqxlsx.__name__: "data/nfq/fix_nfqxlsx/fix_stcvol_nfqxlsx",
+        nfq.xlsx2csv.__name__: "data/nfq/nfqcsv/stcvol_nfqcsv",
     }
 
     TEMP_XLSX_DATE_DIRPATH = "data/temp_xlsx/temp_xlsx_date"
@@ -22,21 +22,21 @@ class Path:
 
 class Constant:
 
-    REPAIR = False
-    XLSX2CSV = True
+    REPAIR = True
+    XLSX2CSV = False
 
-    REMOVE_COLUMN_FOR_XLSX2CSV = []  # 横
+    REMOVE_COLUMN_FOR_XLSX2CSV = [1,2,3,4,5,6,7,8]  # 横
     # if you run "Add_new_xlsx.py", please set [1,2,3,4,5,6,7,8] to this variable
     # if you run "convert_nfq2xlsx.py", please set [] to this variable
-    REMOVE_ROW_FOR_XLSX2CSV = []  # 縦
+    REMOVE_ROW_FOR_XLSX2CSV = [1,2]  # 縦
     # if you run "Add_new_xlsx.py", please set [1,2] to this variable
     # if you run "convert_nfq2xlsx.py", please set [] to this variable
 
-    ACTUAL_RANGE_ROW = {"start": 0, "end": 493}  #
-    KESSANTANSIN_RANGE_ROW = {"start": 2, "end": 493}
-    YUUKAHOUKOKU_RANGE_ROW = {"start": 493, "end": 984}
-    KESSANTANSIN_DATE_ROW = 984
-    YUUKAHOUKOKU_DATE_ROW = 985
+    ACTUAL_RANGE_ROW = {"start": 0, "end": 4} #
+    KESSANTANSIN_RANGE_ROW = {"start": 2, "end": 4} # 
+    YUUKAHOUKOKU_RANGE_ROW = {"start": 5, "end": 7} # 
+    # KESSANTANSIN_DATE_ROW = 984
+    # YUUKAHOUKOKU_DATE_ROW = 985
 
     SUBJECTNAME_COLUMN = 6
     SUBJECTCODE_COLUMN = 7
